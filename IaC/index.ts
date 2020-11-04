3159217d49b5f1624c7fb32e61b29b5f1da3eab0
madawei2699 <madawei2699@gmail.com>
2020-11-04 23:59:13 +0800

    add ec2 spot request

--- IaC/index.ts
+++ IaC/index.ts
@@ -1,81 +1,44 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import * as random from "@pulumi/random";
-import {AuroraMysqlEngine, ServerlessEngine} from "@pulumi/aws/rds";
+import {InstanceType} from "@pulumi/aws/types/enums/ec2";
 
-// Get default VPC
-const vpc = awsx.ec2.Vpc.getDefault();
+// AMI image configuration
+const ec2ImageId = 'ami-00ddb0e5626798373';
+const ec2ImageOwner = '099720109477';
+const ec2InstanceName = "aws-ec2-ubuntu";
 
-// Create an Aurora Serverless MySQL database
-const dbsubnet = new aws.rds.SubnetGroup("dbsubnet", {
-    subnetIds: vpc.publicSubnetIds,
-});
+const pulumiAmi = pulumi.output(aws.getAmi({
+    filters: [{ name: "image-id", values: [ec2ImageId]}],
+    owners: [ec2ImageOwner]
+}));
 
-const dbpassword = new random.RandomString("password", {
-    length: 20,
-});
+// const defaultVpc = awsx.ec2.Vpc.getDefault();
 
-const db = new aws.rds.Cluster("invest", {
-    engine: AuroraMysqlEngine,
-    engineMode: ServerlessEngine,
-    engineVersion: "5.7.12",
-    dbSubnetGroupName: dbsubnet.name,
-    masterUsername: "root",
-    masterPassword: dbpassword.result,
-    scalingConfiguration: {
-        autoPause: true,
-        maxCapacity: 4,
-        minCapacity: 1,
-        secondsUntilAutoPause: 300
+const sshPort = 22
+
+const pulumiSecurityGroup = new aws.ec2.SecurityGroup("pulumi-secgrp", {
+    ingress: [{
+            fromPort: sshPort,
+            toPort: sshPort,
+            protocol: "tcp",
+            cidrBlocks: ["0.0.0.0/0"]
+        }]
     }
-});
+);
 
-// A function to run to connect to our database.
-function queryDatabase(): Promise<void> {
-    return new Promise((resolve, reject) => {
-        var mysql      = require('mysql');
-        var connection = mysql.createConnection({
-            host     : db.endpoint.get(),
-            user     : db.masterUsername.get(),
-            password : db.masterPassword.get(),
-            database : db.databaseName.get(),
-        });
+const ec2Instance = new aws.ec2.SpotInstanceRequest(
+    ec2InstanceName,
+    {
+        instanceType: InstanceType.T2_Micro,
+        ami: pulumiAmi.id,
+        vpcSecurityGroupIds: [pulumiSecurityGroup.id],
+        rootBlockDevice: {
+            deleteOnTermination: false,
+            volumeSize: 10
+        }
+    }
+)
 
-        connection.connect();
-
-        console.log("querying...")
-        connection.query('select aurora_version() as version', function (error: any, results: any, fields: any) {
-            if (error) { reject(error); return }
-            console.log('The aurora version is: ', results[0].version);
-            resolve();
-        });
-
-        connection.end();
-    });
-}
-
-// Create a Lambda within the VPC to access the Aurora DB and run the code above.
-const lambda = new aws.lambda.CallbackFunction("invest-lambda", {
-    vpcConfig: {
-        securityGroupIds: db.vpcSecurityGroupIds,
-        subnetIds: vpc.publicSubnetIds,
-    },
-    policies: [
-        aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole,
-        aws.iam.ManagedPolicy.AWSLambdaFullAccess,
-        aws.iam.ManagedPolicy.AmazonRDSFullAccess
-    ],
-    callback: async(ev) => {
-        console.log(ev);
-
-        await queryDatabase();
-    },
-});
-
-// Export the Function ARN
-export const functionArn = lambda.arn;
-
-// Invoke this with:
-// $ aws lambda invoke --function-name $(pulumi stack output functionArn) out.txt
-// $ pulumi logs -f
+export const publicIp = ec2Instance.publicIp;
+export const publicHostName = ec2Instance.publicDns;
