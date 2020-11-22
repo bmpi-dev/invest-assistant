6bac6299fa2b41b0c1d9b70ec1415de884bb32f8
madawei2699 <madawei2699@gmail.com>
2020-11-22 12:14:04 +0800

    refactor dir construct

--- IaC/aws/db/create_all.sh
+++ IaC/aws/db/create_all.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cd infra
+
 echo "destroy pre-created Pulumi instances"
 pulumi destroy --yes
 
+cd ../ansible
+
 echo "generate EC2 keypair and save private key locally (since Pulumi isn't able to do that now)"
 ansible-playbook keypair.yml
+
+cd ../infra
 
 echo "execute Pulumi to create EC2 instances"
 pulumi up --yes
 
 # when server created, get public dns to .ec2ssh for ansible using
+
+cd ../ansible
 
 echo "Downloading the Ansible role 'mysql' with ansible-galaxy"
 ansible-galaxy install -r requirements.yml -p roles/
